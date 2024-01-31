4720d4e2bddd4dfdefe7aee96f5654b8de7c645c
chrischen24 <156254120+chrischen24@users.noreply.github.com>
2024-01-31 14:47:00 -0800

    Update ArrayEamples.java and ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -13,6 +13,7 @@ public class ArrayTests {
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1 , 2 ,3};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input2));  }
 }
